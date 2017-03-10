ae4b80e17aacd54cf6146d2df9f521b32f602ac2
Christian janze <janze@wiwi.uni-frankfurt.de>
2017-03-10 14:39:14 +0100

    second commit

--- sonar.py
+++ sonar.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
+@app.route("/discover")
+def discover():
+	return render_template('discover.html')
 
 if __name__ == "__main__":
     app.run(debug=True) #remove debug=True in production
