9974838af9727728c27704c91228feca41c33e72
Christian janze <janze@wiwi.uni-frankfurt.de>
2017-03-11 13:00:12 +0100

    third commit

--- sonar.py
+++ sonar.py
@@ -9,5 +9,22 @@ def index():
 def discover():
 	return render_template('discover.html')
 
+@app.route("/share")
+def share():
+	return render_template('share.html')
+
+@app.route("/about")
+def about():
+	return render_template('about.html')
+
+
+@app.route("/login")
+def login():
+	return render_template('login.html')
+
+@app.route("/logout")
+def logout():
+	return render_template('logout.html')
+
 if __name__ == "__main__":
     app.run(debug=True) #remove debug=True in production
